6470c72dbc5306333cf69753e91b63c9c559d3c5
ayushanand18 <kingwarrior880@gmail.com>
2026-01-02 15:26:03 +0530

    fix: fixes for cors

--- internal/http/headers.go
+++ internal/http/headers.go
@@ -13,7 +13,7 @@ func PopulateDefaultServerHeaders(ctx context.Context, r *http.Request, headers 
 	headers["X-Server"] = []string{"crazyhttp"}
 	// relay the origin back since we check for allowed origins, earlier
 	headers["Access-Control-Allow-Origin"] = []string{r.Header.Get("Origin")}
-	headers["Access-Control-Allow-Methods"] = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	headers["Access-Control-Allow-Methods"] = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"}
 	headers["Access-Control-Allow-Headers"] = []string{"Content-Type", "Authorization"}
 	headers["Access-Control-Allow-Credentials"] = []string{"true"}
 	headers["Access-Control-Max-Age"] = []string{"86400"}
